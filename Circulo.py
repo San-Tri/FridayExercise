334d7b0fa8091d9b4fcd2235316e63681e113682
San-Tri <1473024@senati.pe>
2023-03-03 09:48:25 -0500

    AreCirculo.3

--- Circulo.py
+++ Circulo.py
@@ -3,9 +3,9 @@
 # importar la clase math:
 import math
 # ingresar el radio del circulo:
-print("Ingrese el radio del circulo: ")
+print("Ingrese el radio del circulo:")
 # Capturar el radio:
 r = float(input())
 # Optener el valor de pi:
 pi = math.pi
-print("Valor de PI: ",pi)
+print("Valor de PI:",pi)
