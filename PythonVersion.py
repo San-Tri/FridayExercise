3d72c81c3ed8d41d95ec0bb6d195a726cf4d525e
San-Tri <1473024@senati.pe>
2023-03-03 09:11:01 -0500

    PythonVersion.

--- PythonVersion.py
+++ PythonVersion.py
@@ -1,3 +1,8 @@
 # Mostrar la version actual de python:
+
+# Importar el modulo sys:
 import sys
+# imprimir la version de python:
 print(sys.version)
+# Mas detayes de la version de python:
+print(sys.version_info)
